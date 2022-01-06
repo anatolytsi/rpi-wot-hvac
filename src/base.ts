e4be43f0ea6c44da856ff43209cacd9e4d4007ac
anatolytsi <snipl33t@gmail.com>
2022-01-06 13:32:50 +0100

    separated hvac from wot

--- src/base.ts
+++ src/base.ts
@@ -1,61 +1,18 @@
 import '@node-wot/core';
-import {TSensor} from './temperature_sensor';
-import {Valve} from './valve';
-import {AdcConfigs, GpioConfigs} from "./interfaces/types";
+import {Hvac} from "./hvac";
 
 export class WotHvac {
     public thing!: WoT.ExposedThing;
     public td!: WoT.ThingDescription;
     private WoT: WoT.WoT;
-    private t1!: TSensor;
-    private t2!: TSensor;
-    private t3!: TSensor;
-    private t4!: TSensor;
-//    private t5!: TSensor;
-    private valve1!: Valve;
-    private valve2!: Valve;
-    private valve3!: Valve;
-    private valve4!: Valve;
+    private hvac: Hvac;
 
     constructor(WoT: WoT.WoT, tm: any) {
         this.WoT = WoT;
+        this.hvac = new Hvac();
         this.WoT.produce(tm).then(async (exposedThing: WoT.ExposedThing) => {
             this.thing = exposedThing;
             this.td = this.thing.getThingDescription();
-            let adcConfig: AdcConfigs = {
-                type: 'ads1115',
-                i2cDevice: 1,
-                address: 0x48
-            };
-            this.t1 = new TSensor(adcConfig, 0);
-            this.t2 = new TSensor(adcConfig, 1);
-            this.t3 = new TSensor(adcConfig, 2);
-            this.t4 = new TSensor(adcConfig, 3);
-  //          adcConfig.address = 0x49;
-  //          this.t5 = new TSensor(adcConfig, 0);
-            let gpioConfig: GpioConfigs = {
-                type: 'mcp23017',
-                i2cDevice: 1,
-                modeA: 'INPUT_PULLUP',
-                modeB: 'OUTPUT',
-                address: 0x20
-            };
-            this.valve1 = new Valve(
-                {openPin: 8, closePin: 9, openedSwPin: 0, closedSwPin: 1},
-                gpioConfig
-            )
-            this.valve2 = new Valve(
-                {openPin: 10, closePin: 11, openedSwPin: 2, closedSwPin: 3},
-                gpioConfig
-            )
-            this.valve3 = new Valve(
-                {openPin: 12, closePin: 13, openedSwPin: 4, closedSwPin: 5},
-                gpioConfig
-            )
-            this.valve4 = new Valve(
-                {openPin: 14, closePin: 15, openedSwPin: 6, closedSwPin: 7},
-                gpioConfig
-            )
             this.addPropertyHandlers();
             this.addActionHandlers();
             this.addEventHandlers();
@@ -65,71 +22,71 @@ export class WotHvac {
 
     private addPropertyHandlers() {
         this.thing.setPropertyReadHandler('temperature1', async () => {
-            return await this.t1.readTemperature();
+            return await this.hvac.t1.readTemperature();
         });
         this.thing.setPropertyReadHandler('temperature2', async () => {
-            return await this.t2.readTemperature();
+            return await this.hvac.t2.readTemperature();
         });
         this.thing.setPropertyReadHandler('temperature3', async () => {
-            return await this.t3.readTemperature();
+            return await this.hvac.t3.readTemperature();
         });
         this.thing.setPropertyReadHandler('temperature4', async () => {
-            return await this.t4.readTemperature();
+            return await this.hvac.t4.readTemperature();
         });
         this.thing.setPropertyReadHandler('temperature5', async () => {
-//            return await this.t5.readTemperature();
+//            return await this.hvac.t5.readTemperature();
               return 123;
         });
         this.thing.setPropertyReadHandler('valveOpened1', async () => {
-            return await this.valve1.isOpened();
+            return await this.hvac.valve1.isOpened();
         });
         this.thing.setPropertyReadHandler('valveClosed1', async () => {
-            return await this.valve1.isClosed();
+            return await this.hvac.valve1.isClosed();
         });
         this.thing.setPropertyReadHandler('valveOpened2', async () => {
-            return await this.valve2.isOpened();
+            return await this.hvac.valve2.isOpened();
         });
         this.thing.setPropertyReadHandler('valveClosed2', async () => {
-            return await this.valve2.isClosed();
+            return await this.hvac.valve2.isClosed();
         });
         this.thing.setPropertyReadHandler('valveOpened3', async () => {
-            return await this.valve3.isOpened();
+            return await this.hvac.valve3.isOpened();
         });
         this.thing.setPropertyReadHandler('valveClosed3', async () => {
-            return await this.valve3.isClosed();
+            return await this.hvac.valve3.isClosed();
         });
         this.thing.setPropertyReadHandler('valveOpened4', async () => {
-            return await this.valve4.isOpened();
+            return await this.hvac.valve4.isOpened();
         });
         this.thing.setPropertyReadHandler('valveClosed4', async () => {
-            return await this.valve4.isClosed();
+            return await this.hvac.valve4.isClosed();
         });
     }
 
     private addActionHandlers() {
         this.thing.setActionHandler('openValve1', async () => {
-            return this.valve1.open();
+            return this.hvac.valve1.open();
         });
         this.thing.setActionHandler('closeValve1', async () => {
-            return this.valve1.close();
+            return this.hvac.valve1.close();
         });
         this.thing.setActionHandler('openValve2', async () => {
-            return this.valve2.open();
+            return this.hvac.valve2.open();
         });
         this.thing.setActionHandler('closeValve2', async () => {
-            return this.valve2.close();
+            return this.hvac.valve2.close();
         });
         this.thing.setActionHandler('openValve3', async () => {
-            return this.valve3.open();
+            return this.hvac.valve3.open();
         });
         this.thing.setActionHandler('closeValve3', async () => {
-            return this.valve3.close();
+            return this.hvac.valve3.close();
         });
         this.thing.setActionHandler('openValve4', async () => {
-            return this.valve4.open();
+            return this.hvac.valve4.open();
         });
         this.thing.setActionHandler('closeValve4', async () => {
-            return this.valve4.close();
+            return this.hvac.valve4.close();
         });
     }
 
---
+++ src/hvac.ts
@@ -0,0 +1,52 @@
+import {TSensor} from "./temperature_sensor";
+import {Valve} from "./valve";
+import {AdcConfigs, GpioConfigs} from "./interfaces/types";
+
+export class Hvac {
+    public t1: TSensor;
+    public t2: TSensor;
+    public t3: TSensor;
+    public t4: TSensor;
+//    public t5: TSensor;
+    public valve1: Valve;
+    public valve2: Valve;
+    public valve3: Valve;
+    public valve4: Valve;
+
+    constructor() {
+        let adcConfig: AdcConfigs = {
+            type: 'ads1115',
+            i2cDevice: 1,
+            address: 0x48
+        };
+        this.t1 = new TSensor(adcConfig, 0);
+        this.t2 = new TSensor(adcConfig, 1);
+        this.t3 = new TSensor(adcConfig, 2);
+        this.t4 = new TSensor(adcConfig, 3);
+        //          adcConfig.address = 0x49;
+        //          this.t5 = new TSensor(adcConfig, 0);
+        let gpioConfig: GpioConfigs = {
+            type: 'mcp23017',
+            i2cDevice: 1,
+            modeA: 'INPUT_PULLUP',
+            modeB: 'OUTPUT',
+            address: 0x20
+        };
+        this.valve1 = new Valve(
+            {openPin: 8, closePin: 9, openedSwPin: 0, closedSwPin: 1},
+            gpioConfig
+        )
+        this.valve2 = new Valve(
+            {openPin: 10, closePin: 11, openedSwPin: 2, closedSwPin: 3},
+            gpioConfig
+        )
+        this.valve3 = new Valve(
+            {openPin: 12, closePin: 13, openedSwPin: 4, closedSwPin: 5},
+            gpioConfig
+        )
+        this.valve4 = new Valve(
+            {openPin: 14, closePin: 15, openedSwPin: 6, closedSwPin: 7},
+            gpioConfig
+        )
+    }
+}
