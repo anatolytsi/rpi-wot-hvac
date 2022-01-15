d220444be3cbc2c7d9372b9c1038ed2fcd094710
anatolytsi <snipl33t@gmail.com>
2022-01-15 22:47:18 +0100

    added websockets

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const {HttpServer} = require('@node-wot/binding-http');
+const {WebSocketServer} = require('@node-wot/binding-websockets');
 const {Servient} = require('@node-wot/core');
 const {WotHvac} = require('./dist/base');
 
 let httpServer = new HttpServer({port: 8080});
+let webSocketsServer = new WebSocketServer({port: 8081});
 let servient = new Servient();
 servient.addServer(httpServer);
+servient.addServer(webSocketsServer);
 
 servient.start().then(async (WoT) => {
     let hvac = new WotHvac(WoT, require('./wothvac.model.json'));
